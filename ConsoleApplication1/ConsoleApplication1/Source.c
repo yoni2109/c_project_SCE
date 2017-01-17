813646b28281871a1213148d5b67ddd3d6075456
zoharazr <zoharazr@gmail.com>
2017-01-17 21:31:53 +0200

    commit

--- ConsoleApplication1/ConsoleApplication1/Source.c
+++ ConsoleApplication1/ConsoleApplication1/Source.c
@@ -13,14 +13,15 @@
 #define MESSAGE_SIZE 200
 #define TASKS_FILE "tasks.txt"
 #define PROJECT_MANAGERS_FILE "projects_managers.txt"
+#define UNIVERSAL_DIRECTOR "ADMIN"
 
 
 typedef enum{ False = 0, True = 1 } bool;
 typedef struct
 {
 	char sender[SIZE];
-	char content[MESSAGE_SIZE];
-
+	char content[SIZE];
+	char message[MESSAGE_SIZE]
 }Messages;
 typedef struct
 {
@@ -66,10 +67,10 @@ typedef struct
 void print_user_projects(int index_user_array);
 void signUp();
 void cleanBuffer();
-int log_in(char member[]);
+int log_in();
 int String(char arry[]);
 void cleanString(char arry[]);
-int If_Member_Return_True(char user[], char password[]);
+int check_member(char user[], char password[]);
 int compareArrays(char user_from_list[], char user_from_member[]);
 void play(char member[]);
 void system_massage();
@@ -79,6 +80,7 @@ int check_admin(char * name);
 void confirm_project(int index_user);
 void fill_arrays();
 void send_message();
+void Reallocation(struct arr[], char member[], int amount);
 
 WebManager* Wmanager;//will contain the web managet user name
 Users* users_array;// will contain all web users
@@ -96,6 +98,7 @@ int main(){
 	int enter = 0;
 	system_massage(member);
 	
+	
 	do
 	{
 		printf("1.log in\n2.sign up\nplease enter you chooic: ");//זמנית בנתיים עד שנראה איך מריצים דרך פונקציה play
@@ -111,33 +114,20 @@ int main(){
 	return 0;
 }
 void signUp(){
-	int count = 0, member_Exist = False, temp = '\0';
+	int member_Exist = False;
 	char member[SIZE], password[SIZE];//open arry
-	FILE *users;//Declaring files
-	Users *check_user;// Opening indicates the size of the array;
-	users = fopen(USER_FILE_NAME, "r+");//open file to read
-	if (users == NULL){//if file not open quit from program
-		printf("the file could not be opened\n");
-		exit(1);
-	}
-	fscanf(users, "%d", &count);//get the number of users
-	check_user = (Users*)malloc((count)*sizeof(Users));//Opening indicates the size of the array;
 	do//loop for sign up
 	{
 		if (member_Exist){//if user make mistake
 			member_Exist = False;//reset the flag
-			if (wont_exit()){//print to user quiquestion if Continue if not return
-				free(check_user);//free pointer
+			if (wont_exit())//print to user quiquestion if Continue if not return
 				return;
-			}
 		}
 		printf("please enter a new member: ");//print to user Guidelines
-		fseek(users, 0L, SEEK_SET);//go to the start in file
 		member_Exist = String(member);//get the user name from user
 		if (!member_Exist){//if user name is correct get in loop
-			for (int i = 0; i < count && member_Exist == False; i++){//loop for check if user name in system
-				fscanf(users, "%s", &check_user[i].name);//scan from file to pointer
-				if (compareArrays(check_user[i].name, member)){//return true if the user in system
+			for (int i = 0; i < web_projects_amount && member_Exist == False; i++){//loop for check if user name in system
+				if (!strcmp(users_array[i].name, member)){//return true if the user in system
 					printf("this member Exists!\n");
 					member_Exist = True;
 				}
@@ -150,22 +140,15 @@ void signUp(){
 		if (member_Exist){//if user make mistake
 			member_Exist = False;//reset the flag
 			if (wont_exit()){//check if the user want to continue
-				free(check_user);//frre the pointer
 				return;
 			}
 			printf("please enter password again: ");//print to user massage
 		}
 		member_Exist = String(password);//get password from user
 	} while (member_Exist);
-	fseek(users, 0L, SEEK_SET);//go to head of fie
-	fscanf(users, "%d", &count);//get the number of users in file
-	count++;//after we get name and password we update the number of users
-	fseek(users, 0L, SEEK_SET);//go back to head of  file
-	fprintf(users, "%d", count);//print the new number of memeber to file
-	fseek(users, 0L, SEEK_END);//go to end in file
-	fprintf(users, "\n%s %s", member, password);//print to file to new user
-	fclose(users);//close the file
-	free(check_user);//free the pointer
+	realloc(users_array, web_projects_amount+1);
+	strcpy(users_array[web_projects_amount].name, member);
+	strcpy(users_array[web_projects_amount].password, password);
 }
 void cleanBuffer(){//clean the buffer
 	char buffer;
@@ -176,10 +159,9 @@ void cleanString(char arry[]){//If there are more letters than the size of the s
 		arry[i] = '\0';
 	}
 }
-int log_in(char member[]){
+int log_in(){
 	int Not_Valid_Pass = False, Not_Valid_Name = False, Not_Member = True;
-	char temp = '\0';
-	char password[SIZE];//open string
+	char password[SIZE], member[SIZE];//open string
 	do{
 		if (!Not_Member){//if user make mistake 
 			printf("wrong member or password\n");//print error
@@ -207,7 +189,7 @@ int log_in(char member[]){
 			printf("enter your Password maximum chars [%d]: ", SIZE - 1);//Writes the user what to do
 			Not_Valid_Pass = String(password);//Receiving a string if size of letters big then size of arry Raise Flag (notvalid)
 		} while (Not_Valid_Pass);//if notvalid = true -> loop
-		Not_Member = If_Member_Return_True(member, password);//check if appropriate password to use
+		Not_Member = check_member(member, password);//check if appropriate password to use
 	} while (!Not_Member);
 	return True;
 }
@@ -231,37 +213,15 @@ int String(char arry[]){//function to get string for user
 	arry[count] = '\0';//put \0-> end of string in the end of string
 	return False;
 }
-int If_Member_Return_True(char user[], char password[]){
-	int count = 0;
-	FILE *users;//Declaring files
-	Users *check_user; //Open User type structures
-	users = fopen(USER_FILE_NAME, "r");//open file to read
-	if (users == NULL){//if file not open quit from program
-		printf("the file could not be opened\n");
-		exit(1);
-	}
-	fscanf(users, "%d", &count);//get the number of users
-	check_user = (Users*)malloc((count)*sizeof(Users));//Opening indicates the size of the array
-	for (int i = 0; i<count; i++){//loop for check if member exist
-		fscanf(users, "%s%s", &check_user[i].name, &check_user[i].password);//scan users from file
-		if (compareArrays(check_user[i].name, user)){//open function if user exsist check password
-			if (compareArrays(check_user[i].password, password)){//If appropriate password to use
-				fclose(users);//close file
-				free(check_user);//free poinet
+int check_member(char user[], char password[]){
+	for (int i = 0; i < web_users_amount; i++){//loop for check if member exist
+		if (!strcmp(users_array[i].name, user))//open function if user exsist check password
+			if (!strcmp(users_array[i].password, password))//If appropriate password to use
 				return True;
-			}
-			else{
-				fclose(users);//close file
-				free(check_user);//free pointer
-				return False;
-			}
-		}
 	}
-	free(check_user);
-	fclose(users);
 	return False;
 }
-int compareArrays(char user_from_list[], char user_from_member[]) {//Check for identical strings // בודק אם שם משתמש קיים במערכת
+int compareArrays(char user_from_list[], char user_from_member[]) {//Check for identical strings 
 	int i;
 	for (i = 0; user_from_list[i] != '\0' && user_from_member[i] != '\0'; i++) {
 		if (user_from_list[i] != user_from_member[i])
@@ -273,40 +233,8 @@ int compareArrays(char user_from_list[], char user_from_member[]) {//Check for i
 	}
 	return True;
 }
-int ask_if_admin(char member[]){
-	int count = 0, i = 0, Not_admin = True;
-	char temp = '\0';
-	FILE *admin;//Declaring files
-	WebManager *check_admin;
-	admin = fopen(ADMIN_FILE, "r");//open file to read
-	if (admin == NULL){//if file not open quit from program
-		printf("the file could not be opened\n");
-		exit(1);
-	}
-	fscanf(admin, "%d", &count);
-	check_admin = (WebManager*)malloc((count)*sizeof(WebManager));//Opening indicates the size of the array
-	for (int i = 0; i<count&&Not_admin == True; i++){
-		fscanf(admin, "%s", &check_admin[i].name);//scan from file to pointer
-		if (compareArrays(check_admin[i].name, member)){
-			printf("welcome %s you want to conect to your admin account if so enter Y else enter other any key: ", member);
-			scanf("%c", &temp);
-			if (temp == 'Y' || temp == 'y'){
-				Not_admin = False;
-				printf("%s you now Admin\n", member);
-				fclose(admin);
-				free(check_admin);
-				return True;
-			}
-		}
-	}
-	fclose(admin);
-	free(check_admin);
-	if (Not_admin)
-		printf("welcome %s\n", member);
-	return False;
-}
 int wont_exit(){//function to ask the user if exit to loby/main
-	int temp = False;
+	char temp = '\0';
 	printf("you want enter again if so enter Y else enter othe key: ");//massage to user
 	scanf("%c", &temp);//scan flag
 	if (!(temp == 'y' || temp == 'Y')){//if flag = y(yes) continue
@@ -367,8 +295,8 @@ void system_massage(char sender[]){
 		} while (member_Exist);
 		strcpy(system_massage.sender, sender);
 		printf("enter your mesagge: ");
-		fgets(system_massage.massage, MESSAGE_SIZE, stdin);
-		fprintf(message, "%s\n%s\n%s", system_massage.sender, system_massage.content, system_massage.massage);
+		fgets(system_massage.message, MESSAGE_SIZE, stdin);
+		fprintf(message, "%s\n%s\n%s", system_massage.sender, system_massage.content, system_massage.message);
 		fclose(users);
 		fclose(message);
 	}
@@ -459,6 +387,7 @@ void confirm_project(int index_project, char * manager_project){//func to archiv
 		}
 	}
 }
+
 
 
 
